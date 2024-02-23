96597d10c9d29500287ebdc9dd2954ac704079ec
Bartek Bartosik <b.bartek987@gmail.com>
2024-02-23 23:10:45 +0100

    added padding to main container// removed a bold font on number of question label

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -5,22 +5,48 @@ import { View } from "../../types";
 export const Results = () => {
   const { userAnswers, results, onSetView } = useGlobalProvider();
 
-  const correctAnswers = useMemo(() => {
-    const correctAnswersArr = results.map((result) =>
-      result.correct_answer.trim()
-    );
-    return userAnswers.reduce((previous, current, index) => {
-      return correctAnswersArr[index] === current.trim()
-        ? previous + 1
-        : previous;
+  const correctAnswersCount = useMemo(() => {
+    return results.reduce((count, result, index) => {
+      return result.correct_answer.trim() === userAnswers[index]
+        ? count + 1
+        : count;
     }, 0);
   }, [userAnswers, results]);
 
+  const incorrectAnswersCount = useMemo(() => {
+    return userAnswers.length - correctAnswersCount;
+  }, [userAnswers, correctAnswersCount]);
+
+  const totalQuestions = useMemo(() => {
+    return results.length;
+  }, [results]);
+
+  const correctAnswersShare = useMemo(() => {
+    return Math.round((correctAnswersCount / totalQuestions) * 100);
+  }, [correctAnswersCount, totalQuestions]);
+
+  const incorrectAnswersShare = useMemo(() => {
+    return 100 - correctAnswersShare;
+  }, [correctAnswersShare]);
+
   return (
-    <div className="container mx-auto max-w-screen-lg py-8">
-      <h1 className="text-xl font-bold mb-4">
-        Your results: {`${correctAnswers}/${results.length}`}
+    <div className="container mx-auto max-w-screen-lg ">
+      <h1 className="text-xl flex font-bold mb-4 text-white justify-center">
+        Your results: {`${correctAnswersCount}/${totalQuestions}`}
       </h1>
+      <div
+        id="summary-stats "
+        className="text-white flex items-center flex-col"
+      >
+        <p className="bg-green-500 text-center w-1/2">
+          <span className="number ">{correctAnswersShare}%</span>
+          <span className="text"> answered correctly</span>
+        </p>
+        <p className="bg-red-500 text-center w-1/2 my-1">
+          <span className="number">{incorrectAnswersShare}%</span>
+          <span className="text"> answered incorrectly</span>
+        </p>
+      </div>
       {results.map((result, index) => {
         const isCorrect = result.correct_answer.trim() === userAnswers[index];
 
